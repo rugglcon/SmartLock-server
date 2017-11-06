dc738928219d9e64c484fae2631cdeb054cc87bd
Connor Ruggles <cruggles@iastate.edu>
2017-11-05 20:58:17 -0600

    refactor query module into an object for portability

--- server/query.py
+++ server/query.py
@@ -6,58 +6,64 @@ for the data that goes in and out.
 
 import mysql.connector
 
-CONN = mysql.connector.connect(user='pi', password='329db',
-        host='localhost', database='smart_lock')
+class Query:
+    """query class for portability"""
 
-def select(selector, table, where=""):
-    """
-    function to retrieve data from tables
-    returns all the rows from the select
+    def __init__(self, user, password, host, db):
+        self.conn = mysql.connector.connect(user=user, password=password,
+                    host=host, database=db)
 
-    selector - comma separated list of columns to select,
-        or '*' for all
-    table - table or comma separated list of tables to
-        select from
-    where - defaults to empty string to select all rows,
-        otherwise takes 'where' condition for query
-    """
-    cursor = CONN.cursor(buffered=True, dictionary=True)
-    selection = "SELECT {} FROM {}".format(selector, table)
-    if where != "":
-        selection += " WHERE {}".format(where)
-    cursor.execute(selection)
-    return cursor
+    def __del__(self):
+        self.conn.close()
 
-def insert(table, columns, values):
-    """
-    inserts data into a given table
-    returns True for success, False on Error
+    def select(self, selector, table, where=""):
+        """
+        function to retrieve data from tables
+        returns all the rows from the select
 
-    table - string of the table to insert to
-    columns - comma separated list of column names
-    values - comma separated list of values
-    """
-    cursor = CONN.cursor(buffered=True, dictionary=True)
-    query = "INSERT INTO {} ({}) VALUES ({})".format(
-        table, columns, values)
+        selector - comma separated list of columns to select,
+            or '*' for all
+        table - table or comma separated list of tables to
+            select from
+        where - defaults to empty string to select all rows,
+            otherwise takes 'where' condition for query
+        """
+        cursor = self.conn.cursor(buffered=True, dictionary=True)
+        selection = "SELECT {} FROM {}".format(selector, table)
+        if where != "":
+            selection += " WHERE {}".format(where)
+        cursor.execute(selection)
+        return cursor
 
-    try:
-        cursor.execute(query)
-    except mysql.connector.Error:
-        return False
-    return True
+    def insert(self, table, columns, values):
+        """
+        inserts data into a given table
+        returns True for success, False on Error
+
+        table - string of the table to insert to
+        columns - comma separated list of column names
+        values - comma separated list of values
+        """
+        cursor = self.conn.cursor(buffered=True, dictionary=True)
+        query = "INSERT INTO {} ({}) VALUES ({})".format(
+            table, columns, values)
+
+        try:
+            cursor.execute(query)
+        except mysql.connector.Error:
+            return False
+        return True
 
 
-def gen_query(query_string):
-    """
-    general query function for unique queries
-    returns False on Error, otherwise returns
-        the cursor object
-    """
-    cursor = CONN.cursor(buffered=True, dictionary=True)
-    try:
-        cursor.execute(query_string)
-    except mysql.connector.Error:
-        return False
-    finally:
+    def gen_query(self, query_string):
+        """
+        general query function for unique queries
+        returns False on Error, otherwise returns
+            the cursor object
+        """
+        cursor = self.conn.cursor(buffered=True, dictionary=True)
+        try:
+            cursor.execute(query_string)
+        except mysql.connector.Error:
+            return False
         return cursor
