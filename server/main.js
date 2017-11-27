25d79e49b0b5a0be8d2b1468960ba9cd1a541c98
Connor Ruggles <cruggles@iastate.edu>
2017-11-27 00:39:29 -0600

    finish endpoint setup for basic stuff

--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+  request = require('request'),
   query = require('./utils/query.js'),
   auth = require('./utils/auth.js');
 var app = express();
@@ -13,15 +14,55 @@ function handle_res(res, err, data) {
 
 app.get('/open_lock', function(req, res) {
   auth.use_lock(req.query.user_id, req.query.lock_id, 'U',
-    function(err, data) {
-      handle_res(res, err, data);
+  function(err, data) {
+    if(err) {
+      return res.send(JSON.stringify({error: err, data: data}));
+    } else {
+      query.query("select * from Locks where id = " + req.query.lock_id,
+        function(err, data) {
+          if(err) {
+            return res.send(JSON.stringify({error: err, data: data}));
+          } else {
+            var lock_ip = data[0].IP;
+            request.get('http://0.0.0.0:8000' + '/open_lock',
+              function(error, resp, body) {
+                if(error) {
+                  return res.send(JSON.stringify({error: error, data: body}));
+                } else {
+                  console.log(body);
+                  res.send(JSON.stringify({error: 0, data: body}));
+                }
+            });
+          }
+      });
+    }
   });
 });
 
 app.get('/close_lock', function(req, res) {
   auth.use_lock(req.query.user_id, req.query.lock_id, 'L',
     function(err, data) {
-      handle_res(res, err, data);
+      if(err) {
+        return res.send(JSON.stringify({error: err, data: data}));
+      } else {
+        query.query("select * from Locks where id = " + req.query.lock_id,
+          function(err, data) {
+            if(err) {
+              return res.send(JSON.stringify({error: err, data: data}));
+            } else {
+              var lock_ip = data[0].IP;
+              request.get('http://0.0.0.0:8000' + '/close_lock',
+                function(error, resp, body) {
+                  if(error) {
+                    return res.send(JSON.stringify({error: error, data: body}));
+                  } else {
+                    console.log(body);
+                    res.send(JSON.stringify({error: 0, data: body}));
+                  }
+              });
+            }
+        });
+      }
   });
 });
 
